9ea0eefbb956ae5dcb92e72a985549d2531d15db
I A <ilie.adrian1986@gmail.com>
2022-12-01 19:27:38 +0200

    card image added for testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     },
     extend: {
       colors: {
-        bgPrimary: 'rgb(249, 251, 252)',
-        bgSecondary: 'rgb(55, 72, 72)',
-        textSecondary: 'rgb(44, 58, 58)',
+        bgPrimary: 'rgb(249, 251, 252)', //bgPrimary
+        bgSecondary: 'rgb(55, 72, 72)', //bgSecondary
+        textSecondary: 'rgb(44, 58, 58)', //textSecondary
       },
       fontFamily: {
         body:['Rubik', 'sans-serif']
