bb27205db78c6efd1a15244a17cf2855393b6772
I A <ilie.adrian1986@gmail.com>
2023-02-11 22:41:14 +0200

    fp0mr validation tasting

--- js/contactform.js
+++ js/contactform.js
@@ -1,10 +1,41 @@
 const form = document.getElementById('form')
-// const name = document.getElementById('name')
-const formEmail = document.getElementById('react-aria-5')
+const contactName = document.getElementById('name')
+const email = document.getElementById('email')
 const message = document.getElementById('message')
 
 
+function checkRequired (inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+        error(input)
+        } else {
+        succes(input);
+        }   
+    });
+}
+
+function error(input) {
+    const target = document.getElementsByClassName('target');
+    target.className = 'target mt-1 font-medium text-red-400';
+}
+
+function succes(input) {
+    const target = document.getElementsByClassName('target');
+    target.className = 'target hidden mt-1 font-medium text-red-400';
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())){
+        succes(input);
+    } else {
+        error(input);
+    }
+}
+
+
 form.addEventListener("submit", e => {
-    e.preventDefault();
-    console.log(formEmail.value)
+    e.preventDefault()
+    checkRequired([contactName, email, message])
+    checkEmail(email)
 })
